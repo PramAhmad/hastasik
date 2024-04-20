eeaf06f47141bbc7e63a87ffa4a4cb717e069ebc
PramAhmad <pramuditaadit2810@gmail.com>
2024-04-20 22:57:10 +0700

    add co module

--- Modules/UserModule/App/Http/Controllers/CheckOutController.php
+++ Modules/UserModule/App/Http/Controllers/CheckOutController.php
@@ -12,38 +12,31 @@ use Modules\UserModule\App\Models\Customer;
 class CheckOutController extends Controller
 {
 
-    public function checkout(Request $request) {
-        $customerId = Customer::where('user_id', auth()->user()->id)->pluck("id")->first();
-    
-        $selectedProducts = $request->input('products');
-    
-        if (empty($selectedProducts)) {
-            return response()->json([
-                'message' => 'Tidak ada produk yang dipilih',
-                'status' => 400
-            ]);
-        }
-    
-        try {
-    
-            $transaction = [
-                'customer_id' => $customerId,
-                'products' => []
-            ];
-    
-            foreach ($selectedProducts as $product) {
-                $transaction['products'][] = [
-                    'product' => DB::connection("mongodb")->collection("products")->where('_id', $product['product_id'])->first(),
-                    'quantity' => $product['quantity']
-                ];
-            }
-            DB::connection("mongodb")->collection("transactions")->insert($transaction);
-    
-            return response()->json(['message' => 'Checkout berhasil'], 200);
-        } catch (\Exception $e) {
-            return response()->json(['message' => 'Checkout gagal', 'error' => $e->getMessage()], 500);
-        }
+   public function __construct()
+   {
+       $this->middleware('auth:sanctum');
+   }
+   public function getRecipt(Request $request)
+   {
+    // getfrom caart
+    $cartProduct = DB::connection("mongodb")->collection("chart")->where("customer_id", auth()->user()->id)->get();
+    $total = 0;
+    $data = [];
+    foreach ($cartProduct as $key => $value) {
+        $product = DB::connection("mongodb")->collection("products")->where("_id", $value["product_id"])->first();
+        $total += $product["harga"];
+        $data[] = $product;
     }
+    $response = [
+        "message" => "success",
+        "total" => $total,
+        "data" => $data,
+        "status" => 200,
+    ];
+    return response()->json($response);
+   }
+
+//    insert to taransac
     
     
 }    
